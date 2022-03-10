466a69203465d87a3512575be74ac533e79abee2
Artem V. Mayer <mayer1art@gmail.com>
2022-03-11 00:06:37 +0700

    Создание проекта будущего приложения VK

---
+++ VKApp/VKApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKApp
+//
+//  Created by Artem Mayer on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

abd9813412f7260dafb929cb0c6908ffd6c2cedb
Artem V. Mayer <mayer1art@gmail.com>
2022-03-28 20:21:52 +0700

    Реализованы экраны друзей, групп, фотографий и поиска. Проверка пароля и логина

---
+++ VKApp/VKApp/PreviewImageView.swift
@@ -0,0 +1,20 @@
+//
+//  PreviewImageView.swift
+//  VKApp
+//
+//  Created by Artem Mayer on 28.03.2022.
+//
+
+import UIKit
+
+class PreviewImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

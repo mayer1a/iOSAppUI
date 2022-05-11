6a64bf45ee2959a9628e4b1852ea1ccd99f77d28
Artem V. Mayer <mayer1art@gmail.com>
2022-05-12 01:24:38 +0700

    Реализован алфавитный указатель по друзьям. Исправлено автоматическое вычисление количества фотографий

---
+++ VKApp/VKApp/Views/FriendsAlphabetView.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsAlphabetView.swift
+//  VKApp
+//
+//  Created by Artem Mayer on 09.05.2022.
+//
+
+import UIKit
+
+class FriendsAlphabetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

83dcdb292636e27a210f9bf1465715f40047e04d
Artem V. Mayer <mayer1art@gmail.com>
2022-11-23 16:28:17 +0700

    Added links and hashtags definition in the news text cell

--- VKApp/VKApp/Views/Cells/NewsTextCell.swift
+++ VKApp/VKApp/Views/Cells/NewsTextCell.swift
@@ -18,14 +18,56 @@ final class NewsTextCell: UITableViewCell {
 // MARK: - NewsProtocol
 extension NewsTextCell: NewsProtocol {
     func setup<T : NewsCellTypeDataProtocol>(news: T) {
-        newsText?.text = news.newsBody.text
-        
-        guard cellId == showMoreButton?.tag else { return }
+//        newsText?.text = news.newsBody.text
 
-        if newsText?.isOversizeLabel() == true {
-            showMoreButton?.isHidden = false
-        } else {
-            showMoreButton?.isHidden = true
+        let text = news.newsBody.text
+
+        DispatchQueue.global().async {
+            let linkRegex = try? NSRegularExpression(pattern: "(?i)https?:\\/\\/(?:www\\.)?\\S+(?:\\/|\\b)")
+            let hashtagRegex = try? NSRegularExpression(pattern: "(?i)#\\S+(?:\\/|\\b)")
+
+            if let text = text, let linkRegex = linkRegex, let hashtagRegex = hashtagRegex {
+                let attributedString = NSMutableAttributedString(string: text)
+                let textRange = NSRange(location: 0, length: text.count)
+
+                let linksMatches = linkRegex.matches(in: text, range: textRange)
+                let tagsMatches = hashtagRegex.matches(in: text, range: textRange)
+
+                linksMatches.enumerated().forEach { (index, item) in
+                    if let range = Range(item.range, in: text), let url = URL(string: String(text[range])) {
+                        let attributes: [NSAttributedString.Key : Any] = [.link : url]
+                        let urlAttributedString = NSAttributedString(string: String(text[range]), attributes: attributes)
+
+                        attributedString.replaceCharacters(in: item.range, with: urlAttributedString)
+                    }
+                }
+
+                tagsMatches.enumerated().forEach { (index, item) in
+                    if let range = Range(item.range, in: text) {
+                        let attributes: [NSAttributedString.Key : Any] = [.link : String(text[range])]
+                        let urlAttributedString = NSAttributedString(string: String(text[range]), attributes: attributes)
+
+                        attributedString.replaceCharacters(in: item.range, with: urlAttributedString)
+                    }
+                }
+
+                DispatchQueue.main.async { [weak self] in
+                    self?.newsText?.attributedText = attributedString
+
+                    guard self?.cellId == self?.showMoreButton?.tag else { return }
+
+                    if self?.newsText?.isOversizeLabel() == true {
+                        self?.showMoreButton?.isHidden = false
+                    } else {
+                        self?.showMoreButton?.isHidden = true
+                    }
+                }
+            }
         }
+
     }
+}
+
+extension NSAttributedString.Key {
+    static let hyperlink = NSAttributedString.Key("hyperlink")
 }

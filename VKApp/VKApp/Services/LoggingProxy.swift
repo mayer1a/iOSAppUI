fe34b1a561893a742bb06fd4d2657f58f2213d74
Artem V. Mayer <mayer1art@gmail.com>
2022-12-24 18:03:48 +0700

    add using Logger for LoggingProxy

--- VKApp/VKApp/Services/LoggingProxy.swift
+++ VKApp/VKApp/Services/LoggingProxy.swift
@@ -17,28 +17,41 @@ final class LoggingProxy: SessionHelperInterface {
     // MARK: - Private properties
 
     private let sessionHelper: SessionHelperInterface
+    private let logger: Logger
 
     // MARK: - Construction
 
     private init() {
         self.sessionHelper = SessionHelper()
+        self.logger = Logger()
     }
 
     var getFriendsRequest: DataRequest? {
-        sessionHelper.getFriendsRequest
+        let request = sessionHelper.getFriendsRequest
+        logger.addLog("FRIENDS REQUEST")
+
+        return request
     }
 
     // MARK: - Functions
 
     func getFriends(_ completion: @escaping ([User]) -> Void) {
-        sessionHelper.getFriends(completion)
+        sessionHelper.getFriends { [weak self] friends in
+            self?.logger.addLog("FRIENDS REQUEST")
+            completion(friends)
+        }
     }
 
     func getPhotosRequest(id: Int) -> DataRequest? {
-        sessionHelper.getPhotosRequest(id: id)
+        let photos = sessionHelper.getPhotosRequest(id: id)
+        logger.addLog("PHOTOS REQUEST")
+        return photos
     }
 
     func fetchNewsfeed(from specificTime: TimeInterval? = nil, for nextFrom: String? = nil, completion: @escaping ([News]) -> Void) {
-        sessionHelper.fetchNewsfeed(from: specificTime, for: nextFrom, completion: completion)
+        sessionHelper.fetchNewsfeed(from: specificTime, for: nextFrom) { [weak self] newsfeed in
+            self?.logger.addLog("NEWSFEED REQUEST")
+            completion(newsfeed)
+        }
     }
 }

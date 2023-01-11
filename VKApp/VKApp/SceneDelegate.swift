98b887e316b92a0d8edca2c714118af5b74d14c9
Artem V. Mayer <mayer1art@gmail.com>
2023-01-11 20:44:17 +0700

    change SceneDelegate for new LoginScreen init

--- VKApp/VKApp/SceneDelegate.swift
+++ VKApp/VKApp/SceneDelegate.swift
@@ -19,9 +19,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = scene as? UIWindowScene else { return }
         
-        let contentView = Cell(model: UserMock.shared.viewModel)
+//        let contentView = Cell(model: UserMock.shared.viewModel)
 //        let contentView = Cell(model: GroupMock.shared.viewModel)
-//        let contentView = LoginScreen()
+        let contentView = LoginScreen()
 
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = UIHostingController(rootView: contentView)

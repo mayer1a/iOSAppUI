c1ce1b99f2530b93a00b44ab6a5511776d95f0ae
Artem V. Mayer <mayer1art@gmail.com>
2023-01-11 22:10:34 +0700

    change cell padding

--- VKApp/VKApp/Views/Cells/UserGroupCell/Cell.swift
+++ VKApp/VKApp/Views/Cells/UserGroupCell/Cell.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Cell: View {
 
-    // MARK: - Properties
+    // MARK: - Private properties
 
     private var model: CellViewModel
 
@@ -31,15 +31,15 @@ struct Cell: View {
                 .modifier(TitleLabel())
 
         }
-        .padding(.horizontal, 20.0)
+        .padding(.horizontal, 10.0)
     }
 }
 
-struct UserCell_Previews: PreviewProvider {
-    static var previews: some View {
-        Cell(model: UserMock.shared.viewModel)
-    }
-}
+//struct UserCell_Previews: PreviewProvider {
+//    static var previews: some View {
+//        Cell(model: model)
+//    }
+//}
 
 // MARK: - AvatarView
 
@@ -73,53 +73,4 @@ struct AvatarView: View {
 
         }
     }
-}
-
-// MARK: Mock (To see an example display. Will remove)
-
-struct CellViewModel {
-    let id: Int
-    let fullName: String
-    let avatar: URL?
-}
-
-struct UserMock {
-
-    static let shared = UserMock()
-
-    let viewModel: CellViewModel
-
-    init() {
-        viewModel = CellViewModel(id: user.id,
-                                  fullName: "\(user.firstName) \(user.lastName)",
-                                  avatar: URL(string: user.avatar))
-    }
-
-    private let user = User(id: 654234,
-                            firstName: "Alex",
-                            lastName: "Morning",
-                            isClosed: false,
-                            canAccessClosed: false,
-                            avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
-                            blacklisted: 0,
-                            isFriend: 1)
-}
-
-struct GroupMock {
-
-    static let shared = GroupMock()
-
-    let viewModel: CellViewModel
-
-    init() {
-        viewModel = CellViewModel(id: group.id,
-                                  fullName: "\(group.name)",
-                                  avatar: URL(string: group.avatar))
-    }
-
-    private let group = Group(id: -657635,
-                              name: "Photographers",
-                              isMember: 1,
-                              avatar: "https://dvyvvujm9h0uq.cloudfront.net/com/articles/1515135672-shutterstock_284581649.jpg",
-                              isClosed: 0)
 }

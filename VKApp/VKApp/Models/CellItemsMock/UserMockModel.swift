515e095774da4aef89402dd35cc2513b09cefd54
Artem V. Mayer <mayer1art@gmail.com>
2023-01-12 16:35:32 +0700

    add new image links to users mock

--- VKApp/VKApp/Models/CellItemsMock/UserMockModel.swift
+++ VKApp/VKApp/Models/CellItemsMock/UserMockModel.swift
@@ -10,7 +10,7 @@ import Foundation
 struct UserMockModel {
 
     // MARK: - Properties
-    
+
     static let shared = UserMockModel()
 
     let users: [User] = [
@@ -19,7 +19,7 @@ struct UserMockModel {
              lastName: "Morning",
              isClosed: false,
              canAccessClosed: false,
-             avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
+             avatar: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHw%3D&w=1000&q=80",
              blacklisted: 0,
              isFriend: 1),
         User(id: 654235,
@@ -27,7 +27,7 @@ struct UserMockModel {
              lastName: "Arnon",
              isClosed: false,
              canAccessClosed: false,
-             avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
+             avatar: "https://images.ctfassets.net/1wryd5vd9xez/4DxzhQY7WFsbtTkoYntq23/a4a04701649e92a929010a6a860b66bf/https___cdn-images-1.medium.com_max_2000_1_Y6l_FDhxOI1AhjL56dHh8g.jpeg",
              blacklisted: 0,
              isFriend: 1),
         User(id: 654236,
@@ -35,7 +35,7 @@ struct UserMockModel {
              lastName: "Microns",
              isClosed: false,
              canAccessClosed: false,
-             avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
+             avatar: "https://3dnews.ru/assets/external/illustrations/2012/05/15/629311/deusexhuman530_resize.jpg",
              blacklisted: 0,
              isFriend: 1),
         User(id: 654237,
@@ -51,7 +51,7 @@ struct UserMockModel {
              lastName: "Swift",
              isClosed: false,
              canAccessClosed: false,
-             avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
+             avatar: "https://creators-images.vice.com/content-images/contentimage/no-slug/d4d24f28d34addbcb66fb9e86c8276b2.jpg",
              blacklisted: 0,
              isFriend: 1),
         User(id: 654239,
@@ -59,7 +59,7 @@ struct UserMockModel {
              lastName: "Moonie",
              isClosed: false,
              canAccessClosed: false,
-             avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
+             avatar: "https://www.ekhartyoga.com/media/images/articles/content/Square.jpg",
              blacklisted: 0,
              isFriend: 1),
         User(id: 654240,
@@ -67,7 +67,7 @@ struct UserMockModel {
              lastName: "Aberfeld",
              isClosed: false,
              canAccessClosed: false,
-             avatar: "https://upload.wikimedia.org/wikipedia/commons/2/27/Square%2C_Inc_-_Square_Logo.jpg",
+             avatar: "https://i2-prod.mirror.co.uk/incoming/article27085590.ece/ALTERNATES/n615/0_The-International-Festival-quotTattoo-Collection-2016quot-in-Kiev.jpg",
              blacklisted: 0,
              isFriend: 1)
     ]

9847002f9132ce55de5f3fc4b64f8bf185bc7c40
Artem V. Mayer <mayer1art@gmail.com>
2023-01-12 16:40:28 +0700

    add NewsScreen plug view

---
+++ VKApp/VKApp/Views/NewsScreen/NewsScreen.swift
@@ -0,0 +1,22 @@
+//
+//  NewsScreen.swift
+//  VKApp
+//
+//  Created by Artem Mayer on 12.01.2023.
+//
+
+import SwiftUI
+
+struct NewsScreen: View {
+    
+    // MARK: - Properties
+
+    var body: some View {
+        List() {
+            Text("")
+        }
+        .listStyle(.plain)
+        .navigationBarTitle("Новости", displayMode: .inline)
+        .navigationBarBackButtonHidden()
+    }
+}

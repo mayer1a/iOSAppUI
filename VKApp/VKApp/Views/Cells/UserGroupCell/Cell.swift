65be745db4f1a7a11fb4abc071c0202f35e7c98a
Artem V. Mayer <mayer1art@gmail.com>
2023-02-04 20:51:26 +0700

    refactoring code for ProgressView using by timer

--- VKApp/VKApp/Views/Cells/UserGroupCell/Cell.swift
+++ VKApp/VKApp/Views/Cells/UserGroupCell/Cell.swift
@@ -39,33 +39,67 @@ struct Cell: View {
 
 struct AvatarView: View {
 
+    // MARK: - State properties
+
+    @State var isClosed = false
+
     // MARK: - Properties
 
     let url: URL?
 
     var body: some View {
-        AsyncImage(url: url) { phase in
-            switch phase {
-            case .empty:
-                ZStack {
+        ZStack {
+            AsyncImage(url: url) { phase in
+                switch phase {
+                case .empty, .failure:
                     CircleImage {
                         Image("NonAvatar")
                     }
+                case .success(let image):
+                    CircleImage {
+                        image
+                    }
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            .onAppear {
+                isClosed = true
+            }
 
-                    ProgressView()
-                        .frame(width: 72, height: 72)
-                }
-            case .success(let image):
-                CircleImage {
-                    image
-                }
-            case .failure:
-                CircleImage {
-                    Image("NonAvatar")
-                }
-            @unknown default:
-                EmptyView()
+            if !isClosed {
+                TimerProgressView(isClosed: $isClosed)
             }
         }
+
+    }
+
+}
+
+// MARK: - ProgressView
+
+struct TimerProgressView: View {
+
+    // MARK: - State properties
+
+    @Binding var isClosed: Bool
+
+    // MARK: - Private properties
+
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+    // MARK: - Properties
+
+    var body: some View {
+        let startTime = Date().timeIntervalSince1970
+
+        ProgressView()
+            .frame(width: 72, height: 72)
+            .onReceive(timer) { time in
+                if time.timeIntervalSince1970 > startTime + 3 {
+                    isClosed = true
+                    timer.upstream.connect().cancel()
+                }
+            }
     }
 }

83c36a7eb93fd42ff86e241985535fba9ec7dc49
Artem V. Mayer <mayer1art@gmail.com>
2023-02-10 14:26:35 +0700

    rebase item view to a separate view with geometry matched effect

--- VKApp/VKApp/Views/FriendsPhotosScreen/FriendsPhotosScreen.swift
+++ VKApp/VKApp/Views/FriendsPhotosScreen/FriendsPhotosScreen.swift
@@ -11,9 +11,12 @@ import SwiftUI
 
 struct FriendsPhotosScreen: View {
 
+    @Namespace var namespace
+
     // MARK: - State properties
 
     @State private var itemHeight: CGFloat = 72.0
+    @State private var currentSelection: Int? = nil
 
     // MARK: - Observed properties
 
@@ -38,34 +41,32 @@ struct FriendsPhotosScreen: View {
     var body: some View {
         ScrollView {
             LazyVGrid(columns: column, spacing: 10.0) {
-                ForEach(viewModel.detachedPhotos, id: \.id) { photo in
-                    ZStack {
-                        getGeometryView(for: photo)
+                let ids = viewModel.detachedPhotos.map { $0.id }
 
-                        LikeView(likesCount: photo.likesCounter ?? 0, isLiked: photo.isLiked ?? false)
+                ForEach(viewModel.detachedPhotos, id: \.id) { photo in
+                    if let index = ids.firstIndex(of: photo.id) {
+                        ItemView(itemHeight: $itemHeight,
+                                 selectionIndex: $currentSelection,
+                                 photo: photo,
+                                 cellIndex: index)
+                        .matchedGeometryEffect(id: index, in: namespace, isSource: true)
                     }
                 }
             }
             .onPreferenceChange(HeightPreferenceKey.self) {
                 itemHeight = $0
             }
+            .overlay {
+                if let currentSelection = currentSelection {
+                    SelectionRectangleGeometryEffect(sizeSide: $itemHeight)
+                        .matchedGeometryEffect(id: currentSelection, in: namespace, isSource: false)
+                }
+            }
             .padding([.horizontal, .top], 10)
         }
         .navigationTitle("Фотографии")
         .navigationBarTitleDisplayMode(.inline)
         .onAppear(perform: viewModel.fetchPhotos)
-    }
-
-    // MARK: - Private functions
-
-    private func getGeometryView(for photo: Photo) -> some View {
-        return GeometryReader { geometry in
-            let photoURL = URL(string: photo.originalSizeUrl ?? "")
-            PreviewPhotoView(url: photoURL)
-                .preference(key: HeightPreferenceKey.self, value: geometry.size.width)
-        }
-        .clipped()
-        .frame(height: itemHeight)
     }
 }
 
@@ -138,17 +139,92 @@ struct LikeView: View {
     }
 }
 
-// MARK: - Extension
+struct ItemView: View {
 
-private extension FriendsPhotosScreen {
+    @Binding var itemHeight: CGFloat
+    @Binding var selectionIndex: Int?
 
-    // MARK: - HeightPreferenceKey
+    private let photo: Photo
+    private let cellIndex: Int
+    private let itemAtRow: Int = 3
 
-    struct HeightPreferenceKey: PreferenceKey {
-        static let defaultValue: CGFloat = 0
+    init(itemHeight: Binding<CGFloat>, selectionIndex: Binding<Int?>, photo: Photo, cellIndex: Int) {
+        self._itemHeight = itemHeight
+        self._selectionIndex = selectionIndex
+        self.photo = photo
+        self.cellIndex = cellIndex
+    }
 
-        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-            value = max(value, nextValue())
+    var body: some View {
+        ZStack {
+            getGeometryView(for: photo)
+
+            LikeView(likesCount: photo.likesCounter ?? 0, isLiked: photo.isLiked ?? false)
         }
+    }
+
+    // MARK: - Private functions
+
+    private func getGeometryView(for photo: Photo) -> some View {
+        return GeometryReader { geometry in
+            let photoURL = URL(string: photo.originalSizeUrl ?? "")
+            PreviewPhotoView(url: photoURL)
+                .preference(key: HeightPreferenceKey.self, value: geometry.size.width)
+                .anchorPreference(key: SelectionPreferenceKey.self, value: .bounds) {
+                    self.cellIndex == self.selectionIndex ? $0 : nil
+                }
+                .onTapGesture {
+                    self.selectionIndex = cellIndex
+                }
+        }
+        .clipped()
+        .frame(height: itemHeight)
+    }
+
+}
+
+// MARK: - SelectionRectange
+
+struct SelectionRectangleGeometryEffect: View {
+
+    // MARK: - State properties
+
+    @Binding var sizeSide: CGFloat
+
+    // MARK: - Constructions
+
+    init(sizeSide: Binding<CGFloat>) {
+        self._sizeSide = sizeSide
+    }
+
+    // MARK: - Properties
+
+    var body: some View {
+        GeometryReader { proxy in
+                Rectangle()
+                    .fill(.clear)
+                    .border(LinearGradient.selectionBorderLinearGradient, width: 2)
+                    .frame(width: sizeSide, height: sizeSide)
+        }
+    }
+}
+
+// MARK: - SelectionPreferenceKey
+
+struct SelectionPreferenceKey: PreferenceKey {
+    static var defaultValue: Anchor<CGRect>? = nil
+
+    static func reduce(value: inout Anchor<CGRect>?, nextValue: () -> Anchor<CGRect>?) {
+        value = value ?? nextValue()
+    }
+}
+
+// MARK: - HeightPreferenceKey
+
+struct HeightPreferenceKey: PreferenceKey {
+    static let defaultValue: CGFloat = 0
+
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = max(value, nextValue())
     }
 }
